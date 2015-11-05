7a662d7572a42dabd9eba3168103e5a2c74642b5
Muhammad Arif Ramadhan <arifadhan17@gmail.com>
2015-11-05 13:38:05 +0700

    Tugas 1

---
+++ Tugas 1/02-Pool-Kendaraan/Becak.java
@@ -0,0 +1,16 @@
+public class Becak implements Kendaraan
+{
+    private String plat;
+
+    public Becak(String plat){
+        this.plat=plat;
+    }
+    
+    public String plat(){
+        return plat;
+    }
+    
+    public String tipe(){
+        return "Becak";
+    }
+}
---
+++ Tugas 1/03-Kebun-Binatang/Harimau.java
@@ -0,0 +1,13 @@
+public class Harimau implements Mamalia
+{
+    public String suara()
+    {
+       return "Aum";
+    }
+
+    public int jumlahKaki()
+    {
+        return 4;
+    }
+}
+
